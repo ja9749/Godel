ee21024650457a2f9cf50dd5e816e22bffa1d0fa
ja9749 <ja9749@my.bristol.ac.uk>
2022-01-30 22:24:38 +0000

    Add framework

--- src/App.js
+++ src/App.js
@@ -3,21 +3,17 @@ import './App.css';
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-        </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-        </a>
+        <div className = "app">
+            <header className = "header">
+                <h1>{"Gödel"}</h1>
+                <p>{"A wordle game for gamers"}</p>
             </header>
+            <div className = "game">
+                <p>{"Gamebox"}</p>
+            </div>
+            <div className = "keyboard">
+                <p>{"Keyboard"}</p>
+            </div>
         </div>
     );
 }
